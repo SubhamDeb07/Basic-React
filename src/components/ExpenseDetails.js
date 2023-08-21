e9aeb5d9264577c7e0cc05e0352a409f6ee6679a
Subham <subhamdeb409@gmail.com>
2023-08-21 19:30:36 +0530

    update amount

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,11 +1,21 @@
+import React, {useState} from 'react'
 import './ExpenseDetails.css'
 function ExpenseDetails(props){
+    const [item,setTitle] = useState(props.item)
+    const [amount,setAmount] = useState(props.amount)
+    const clickHandler = () =>{
+        setTitle('Updated')
+        setAmount('100$')
+    }
     return (
         <div className="expense-item__description">
-        <h2>{props.item}</h2>
+        <h2>{item}</h2>
         <h2>{props.location}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">{amount}</div>
+        <button onClick={clickHandler}>Edit</button>
     </div>
+    
+    
     )
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
+
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 import './ExpenseItem.css'
 function ExpenseItem(props){
-    const clickHandler = () =>{
-        console.log("clicked!!")
-    }
+  
     return (
     <div className="expense-item">
     <ExpenseDate date={props.date}/>
    
         <ExpenseDetails item={props.item} location={props.location} amount={props.amount}/>
-        <button onClick={clickHandler}>Delete</button>
     </div>
     
     )
